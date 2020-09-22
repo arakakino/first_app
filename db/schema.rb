7906920019094516b6966db2340c66ad31118da5
arakakino <etffxfang@gmail.com>
2020-09-22 11:52:14 +0900

    no.2

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,4 @@ ActiveRecord::Schema.define(version: 2020_08_23_141301) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
-  
 end
